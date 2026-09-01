743af2ad48d12f613d6894894e3ce82571b0ff69
Naveen Patil <patilnaveen932@gmail.com>
2026-09-01 18:48:36 +0530

    Implement maxDepth function for binary tree

--- binary-trees/maximum-depth.py
+++ binary-trees/maximum-depth.py
@@ -1,1 +1,7 @@
+# LeetCode provides TreeNode.
 
+class Solution:
+    def maxDepth(self, root):
+        if not root:
+            return 0
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
